67e405e3356e73475cea853f36bf24d927416b7c
wskong <63302940+bis0908@users.noreply.github.com>
2022-11-11 11:39:39 +0900

    commit 22.11.11

--- public/js/server.js
+++ public/js/server.js
@@ -1,11 +1,11 @@
-const http = require("http")
-const https = require("https")
-const fs = require("fs")
-
-var privateKey = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/privkey.pem")
-var certificate = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/cert.pem")
-var ca = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/chain.pem")
-const credentials = { key: privateKey, cert: certificate, ca: ca }
+// const http = require("http")
+// const https = require("https")
+// const fs = require("fs")
+//
+// var privateKey = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/privkey.pem")
+// var certificate = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/cert.pem")
+// var ca = fs.readFileSync("/etc/letsencrypt/live/seedconsulting.co.kr/chain.pem")
+// const credentials = { key: privateKey, cert: certificate, ca: ca }
 
 const express = require('express');
 const app = express();
@@ -15,12 +15,12 @@ app.use(express.static('public'));
 // const client = require('') // DB
 app.set('view engine', 'ejs');
 
-// app.listen(80, function () {
-//     console.log('listening on port 80');
-// });
+app.listen(80, function () {
+    console.log('listening on port 80');
+});
 
 // http.createServer(app).listen(80)
-https.createServer(credentials, app).listen(443)
+// https.createServer(credentials, app).listen(443)
 
 // main page
 app.get('/', (req, res) =>{
@@ -34,3 +34,8 @@ app.get('/consulting', (req, res) =>{
 app.get('/reference_table', (req, res) =>{
     res.render('reference_table');
 })
+
+app.get('/test', (req, res) =>{
+    res.render('test');
+})
+
