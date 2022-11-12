eaeeb00c84ef5c9ccd38f54307d27b35c0d1b66c
wskong <63302940+bis0908@users.noreply.github.com>
2022-11-12 11:43:48 +0900

    commit 22.11.12

--- server.js
+++ server.js
@@ -9,10 +9,30 @@ let authInfo;
 // const credentials = { key: privateKey, cert: certificate, ca: ca }
 
 // nodemailer.createTransport에 필요한 접속 정보 json parsing
-fs.readFile('auth.json', (err, data) => {
-    if (err) {throw err}
-    authInfo = JSON.parse(data);
+fs.readFile('./auth.json', (err, data) => {
+    if (err) {
+        throw err
+    }
+    authInfo = JSON.parse(data.toString());
+    console.log("authInfo: "+ authInfo);    // data 읽어옴
 })
+
+console.log("authInfo: "+ authInfo);    // undefined
+
+const smtpTransport = nodemailer.createTransport({
+    host: authInfo.hostInfo,
+    port: authInfo.portInfo,
+    service: authInfo.service,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        user: authInfo.email,
+        pass: authInfo.passwd
+    },
+    logger: true
+});
+
+console.log("smtpTransport: " + smtpTransport);
 
 const express = require('express');
 const app = express();
@@ -25,14 +45,14 @@ const consulting = "consulting";
 const reference_table = "reference_table";
 const test = "test";
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 // const client = require('') // DB
 app.set('view engine', 'ejs');
 
 let corsOptions = {
     // origin: 'https://seedconsulting.co.kr/', // 실서버 구동시 주석 해제
-    origin: 'localhost',    // 로컬 작업시 사용
+    origin: '*',    // 로컬 작업시 사용
     credentials: true
 }
 
@@ -43,36 +63,37 @@ app.listen(port, function () {
     console.log(`listening on port ${port}`);
 });
 
-const smtpTransport = nodemailer.createTransport({
-    host: authInfo.hostInfo,
-    port: authInfo.portInfo,
-    service: authInfo.service,
-    secure: false,
-    requireTLS: true,
-    auth:{
-        user: authInfo.email,
-        pass: authInfo.passwd
-    },
-    logger: true
-});
 
 // http.createServer(app).listen(80)
 // https.createServer(credentials, app).listen(443)
 
 // main page
-app.get('/', (req, res) =>{
-        res.render(index);
+app.get('/', (req, res) => {
+    res.render(index);
 })
 
-app.get('/consulting', (req, res) =>{
+app.get('/consulting', (req, res) => {
     res.render(consulting);
 })
 
-app.get('/reference_table', (req, res) =>{
+app.get('/reference_table', (req, res) => {
     res.render(reference_table);
 })
 
-app.get('/test', (req, res) =>{
+app.get('/test', (req, res) => {
     res.render(test);
 })
 
+app.get('/mail', async (req, res) => {
+    const info = await smtpTransport.sendMail({
+        from: '"mySelf" <seedconsulting2022@gmail.com>',
+        to: "seedconsulting2022@gmail.com",
+        subject: "Hello from node",
+        text: "Hello world?",
+        html: "<strong>Hello world?</strong>",
+        headers: {'x-myheader': 'test header'}
+    });
+
+    console.log("Message sent: %s", info.response);
+    res.send('Hello world!')
+})
